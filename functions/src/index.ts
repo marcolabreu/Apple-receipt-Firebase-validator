db4bf7929275ca405d13bd46d60f6508ac43ba87
Marco L. Abreu <marcolabreu@royalmail.top>
2020-06-10 17:19:51 +0200

    Add prettier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,36 +1,38 @@
-import { https } from 'firebase-functions'
-import bent = require("bent")
+import { https } from "firebase-functions";
+import bent = require("bent");
 
 const sendToApple = async (data: string) => {
-	const body = JSON.stringify({ 'receipt-data': data })
-	const post = bent('POST', 'json', 200)
+  const body = JSON.stringify({ "receipt-data": data });
+  const post = bent("POST", "json", 200);
 
-	/* We try production following Apple recommendation */
-	try {
-		const productionResponse: bent.Json & { status?: number } = await post('https://buy.itunes.apple.com/verifyReceipt', body)
+  /* We try production following Apple recommendation */
+  try {
+    const productionResponse: bent.Json & { status?: number } = await post(
+      "https://buy.itunes.apple.com/verifyReceipt",
+      body
+    );
 
-		/* If status is 21007, we try sandbox */
-		if (productionResponse.status === 21007) {
-			return await post('https://sandbox.itunes.apple.com/verifyReceipt', body)
-		}
+    /* If status is 21007, we try sandbox */
+    if (productionResponse.status === 21007) {
+      return await post("https://sandbox.itunes.apple.com/verifyReceipt", body);
+    }
 
-		return productionResponse
-	} catch (e) {
-		console.error(e);
-		return { 'error': JSON.stringify(e) }
-	}
-}
+    return productionResponse;
+  } catch (e) {
+    console.error(e);
+    return { error: JSON.stringify(e) };
+  }
+};
 
 export const validateReceipt = https.onRequest(async (req, res) => {
-	const appleResponse = await sendToApple(req.body)
+  const appleResponse = await sendToApple(req.body);
 
-	if (appleResponse.status === 0) {
-		console.info(
-			`Receipt for app ${appleResponse.receipt.bundle_id} decrypted by Apple ${appleResponse.environment} server.`
-		)
-	}
+  if (appleResponse.status === 0) {
+    console.info(
+      `Receipt for app ${appleResponse.receipt.bundle_id} decrypted by Apple ${appleResponse.environment} server.`
+    );
+  }
 
-	res.header("Content-Type", "application/json")
-	res.send(JSON.stringify(appleResponse))
-})
-
+  res.header("Content-Type", "application/json");
+  res.send(JSON.stringify(appleResponse));
+});
